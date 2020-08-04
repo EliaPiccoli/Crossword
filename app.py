2799431489dd508130dc6f796e7adeb6d70b59ff
NicolaGugole <nicola.gugole@gmail.com>
2020-08-04 11:53:38 +0200

    Added HTML crossword table generation

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, redirect, url_for, render_template, request
+from crossword import create_crossword
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    if request.method =="POST":
+    if request.method == "POST":
         #TODO call script in crossword.py for crossword generation
-        print(request.form["words"])
-        return "ciao belin" #render_template("crossword.html")
+        crossword_matrix = create_crossword(request.form["words"])
+        return render_template("crossword_page.html", crossword = crossword_matrix)
     else:
         return render_template("main_page.html")
 
--- crossword.py
+++ crossword.py
@@ -2,8 +2,10 @@ import string
 import numpy as np #my love
 import time
 
-def _get_words():
-	return [line.strip() for line in open("words.txt")]
+def _get_words(text):
+	#return [line.strip() for line in open("words.txt")]
+	print("This is the text", text)
+	return [line.strip() for line in text.split("\n")]
     #return ["emergente", "ciao", "ramo", "sole", "luce"]
 
 def _print_crossword(field, size, empty="-"):
@@ -48,137 +50,139 @@ def _debug(field, size, placements, stats):
 	print(stats)
 	x = input()
 
-start = time.time()
+def create_crossword(text):
+	start = time.time()
 
-#dictionary with all the words
-w = _get_words()
-w.sort(reverse=True, key=len)
-print(w)
+	#dictionary with all the words
+	w = _get_words(text)
+	w.sort(reverse=True, key=len)
+	print(w)
 
-size = 2*len(w[0])
-field = [["" for _ in range(size)] for _ in range(size)]
+	size = 2*len(w[0])
+	field = [["" for _ in range(size)] for _ in range(size)]
 
-# dictionary with the info of the words placement
-# (row, col, vertical(True)-horizontal(False))
-placements = {}
-# dictionary with edges index
-edges = {}
-# dictionary with info about the word
-stats = {word:0 for word in w}
+	# dictionary with the info of the words placement
+	# (row, col, vertical(True)-horizontal(False))
+	placements = {}
+	# dictionary with edges index
+	edges = {}
+	# dictionary with info about the word
+	stats = {word:0 for word in w}
 
-#insert first word top-left corner
-for i in range(len(w[0])):
-	field[i][0] = w[0][i]
-placements[w[0]] = (0, 0, True)
+	#insert first word top-left corner
+	for i in range(len(w[0])):
+		field[i][0] = w[0][i]
+	placements[w[0]] = (0, 0, True)
 
-#initialize current crossword edges, useful for later shifting
-# TODO: add edge control
-edges["top"] = 0
-edges["bottom"] = len(w[0]) - 1
-edges["left"] = 0
-edges["right"] = 0
+	#initialize current crossword edges, useful for later shifting
+	# TODO: add edge control
+	edges["top"] = 0
+	edges["bottom"] = len(w[0]) - 1
+	edges["left"] = 0
+	edges["right"] = 0
 
-#initialize two sets that will be updated and represents the status of the words
-used = set(w[0:1])
-remaining = set(w[1:])
-#_print_sets(used, remaining)
+	#initialize two sets that will be updated and represents the status of the words
+	used = set(w[0:1])
+	remaining = set(w[1:])
+	#_print_sets(used, remaining)
 
-while len(remaining) > 0:
-	#print("words left: {}".format(len(remaining)))
-	inserted_new_word = False
+	while len(remaining) > 0:
+		#print("words left: {}".format(len(remaining)))
+		inserted_new_word = False
 
-	for word in remaining:
-		#print(word)
-		#create a list of the placed words sorted by the value of stats -> first we check words with no link to others
-		words_placed = list(used)
-		words_placed.sort(key=lambda x:stats[x])
-		#print(words_placed)
-		for word_placed in words_placed:
-			for c in word:
-				#print(c, word, word_placed)
-				if c in word_placed:
-					#possible link
-					#print(f"Found a possible link between {word} and {word_placed} due to common character '{c}'")
-					if placements[word_placed][2]: #vertical word
-						row = word_placed.find(c) + placements[word_placed][0]
-						col = placements[word_placed][1]
-						pos = word.find(c)
+		for word in remaining:
+			#print(word)
+			#create a list of the placed words sorted by the value of stats -> first we check words with no link to others
+			words_placed = list(used)
+			words_placed.sort(key=lambda x:stats[x])
+			#print(words_placed)
+			for word_placed in words_placed:
+				for c in word:
+					#print(c, word, word_placed)
+					if c in word_placed:
+						#possible link
+						#print(f"Found a possible link between {word} and {word_placed} due to common character '{c}'")
+						if placements[word_placed][2]: #vertical word
+							row = word_placed.find(c) + placements[word_placed][0]
+							col = placements[word_placed][1]
+							pos = word.find(c)
 
-						#print("row {} col {} pos {}".format(row, col, pos))
+							#print("row {} col {} pos {}".format(row, col, pos))
 
-						#check if word fits in current crossword boundaries 
-						if col-pos < 0 or col+(len(word)-pos) > size:
-							#print("We gotta shift brother")
-							field = _shift(field, pos-col, 1)
-							_update_placements(placements, pos-col, True)
-							col = pos
+							#check if word fits in current crossword boundaries 
+							if col-pos < 0 or col+(len(word)-pos) > size:
+								#print("We gotta shift brother")
+								field = _shift(field, pos-col, 1)
+								_update_placements(placements, pos-col, True)
+								col = pos
 
-						#check if fits in needed cells
-						fit = _check_fit(field, row, col, pos, word_placed, word, placements[word_placed][2])
+							#check if fits in needed cells
+							fit = _check_fit(field, row, col, pos, word_placed, word, placements[word_placed][2])
 
-						if fit:
-							for k in range(0, len(word)):
-								field[row][col+k-pos] = word[k]
-							placements[word] = (row, col-pos, False)
+							if fit:
+								for k in range(0, len(word)):
+									field[row][col+k-pos] = word[k]
+								placements[word] = (row, col-pos, False)
 
-							#add placed word to correct set and remove from remaining
-							used.add(word)
-							remaining.remove(word)
+								#add placed word to correct set and remove from remaining
+								used.add(word)
+								remaining.remove(word)
 
-							#pretty printing cause we fenoch
-							#print(f"Updated sets after inserting word {word}")
-							#_print_sets(used,remaining)
-							
-							stats[word_placed] = stats[word_placed] + 1 
-							inserted_new_word = True
-							break
-					else: #horizontal word
-						row = placements[word_placed][0]
-						col = word_placed.find(c) + placements[word_placed][1]
-						pos = word.find(c)
+								#pretty printing cause we fenoch
+								#print(f"Updated sets after inserting word {word}")
+								#_print_sets(used,remaining)
+								
+								stats[word_placed] = stats[word_placed] + 1 
+								inserted_new_word = True
+								break
+						else: #horizontal word
+							row = placements[word_placed][0]
+							col = word_placed.find(c) + placements[word_placed][1]
+							pos = word.find(c)
 
-						#print("row {} col {} pos {}".format(row, col, pos))
+							#print("row {} col {} pos {}".format(row, col, pos))
 
-						#check if word fits in current crossword boundaries 
-						if row-pos < 0 or row+(len(word)-pos) > size:
-							#print("We gotta shift brother")
-							field = _shift(field, pos-row, 0)
-							_update_placements(placements, pos-row, False)
-							row=pos
+							#check if word fits in current crossword boundaries 
+							if row-pos < 0 or row+(len(word)-pos) > size:
+								#print("We gotta shift brother")
+								field = _shift(field, pos-row, 0)
+								_update_placements(placements, pos-row, False)
+								row=pos
 
-						#check if fits in needed cells
-						fit = _check_fit(field, row, col, pos, word_placed, word, placements[word_placed][2])
+							#check if fits in needed cells
+							fit = _check_fit(field, row, col, pos, word_placed, word, placements[word_placed][2])
 
-						if fit:
-							for k in range(0, len(word)):
-								field[row+k-pos][col] = word[k]
-							placements[word] = (row-pos, col, True)
+							if fit:
+								for k in range(0, len(word)):
+									field[row+k-pos][col] = word[k]
+								placements[word] = (row-pos, col, True)
 
-							#add placed word to correct set and remove from remaining
-							used.add(word)
-							remaining.remove(word)
+								#add placed word to correct set and remove from remaining
+								used.add(word)
+								remaining.remove(word)
 
-							#pretty printing cause we fenoch
-							#print(f"Updated sets after inserting word {word}")
-							#_print_sets(used,remaining)
+								#pretty printing cause we fenoch
+								#print(f"Updated sets after inserting word {word}")
+								#_print_sets(used,remaining)
 
-							stats[word_placed] = stats[word_placed] + 1
-							inserted_new_word = True
-							break
-			# TODO: change this trash
+								stats[word_placed] = stats[word_placed] + 1
+								inserted_new_word = True
+								break
+				# TODO: change this trash
+				if inserted_new_word:
+					break
 			if inserted_new_word:
-				break
-		if inserted_new_word:
-				break
-	else:
-		print("Could not create a full crossword")
-		print("These words could not be introduced: ", remaining)
-		exit(1)
+					break
+		else:
+			print("Could not create a full crossword")
+			print("These words could not be introduced: ", remaining)
+			exit(1)
 
-print("FINAL CROSSOWORD")
-_print_crossword(field, size, " ")
+	print("FINAL CROSSOWORD")
+	_print_crossword(field, size, " ")
 
-print("Execution time: {:.3f}".format(time.time() - start))
+	print("Execution time: {:.3f}".format(time.time() - start))
+	return field
 
 # TODO
 # 1. shift field then check for word, if it doesnt fit it wont unshift the field
